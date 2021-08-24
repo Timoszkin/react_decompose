0a25348f8c1cc4c6e6d5cf0def916f6827d8646d
Tymofii Fesenko <tymofii.fesenko1999@gmail.com>
2021-08-24 15:17:45 +0200

    Updates

---
+++ precache-manifest.82a1d767b32c8ff08d7898d74a824185.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b95f8e572b4864f235bbb5156fe2ee",
+    "url": "./index.html"
+  },
+  {
+    "revision": "10b12e9896e718428a7b",
+    "url": "./static/css/main.e5659470.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10b12e9896e718428a7b",
+    "url": "./static/js/main.db0f2b89.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
